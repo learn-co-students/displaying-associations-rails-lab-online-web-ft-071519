22efc40789eaf354c9df694179e0ac614ae7c7fb
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-09-27 12:01:13 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,20 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
+
+    Song.destroy_all
+    Artist.destroy_all
+
+    artist_1 = Artist.create(name: "Emma")
+    artist_2 = Artist.create(name: "Holly")
+    artist_3 = Artist.create(name: "Sophie")
+    
+    song_1 = Song.create(title: "ABC", artist_id: artist_1.id)
+    song_2 = Song.create(title: "Impossible", artist_id: artist_1.id)
+    song_3 = Song.create(title: "Love Song", artist_id: artist_2.id)
+    song_4 = Song.create(title: "Friendship", artist_id: artist_3.id)
+
+
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
